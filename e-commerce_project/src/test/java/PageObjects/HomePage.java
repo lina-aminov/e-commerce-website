effefde89ace3378760d0985ee016aa7f9caaafd
david.sanusi <omotoluwani-david.sanusi@ten10.com>
2023-02-20 17:17:42 +0000

    Added Registration page/feature/stepdef for the logging in requirements

--- e-commerce_project/src/test/java/PageObjects/HomePage.java
+++ e-commerce_project/src/test/java/PageObjects/HomePage.java
@@ -1,14 +1,25 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
-    public void goToHomePage(){
+    public void goToHomePage() {
         driver.get("http://3.11.77.136/index.php");
+    }
+
+    public void clickSignIn() {
+        WebElement signInButton = driver.findElement(By.xpath("//div[@class='user-info']/a"));
+        signInButton.click();
+    }
+    public void clickSignUpLink() {
+        WebElement signUpLink = driver.findElement(By.xpath("//a[contains(@href,'authentication')]"));
+        signUpLink.click();
     }
 }
