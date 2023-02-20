813501a46fa46924007881a11708aacfa5c5767f
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-20 15:42:59 +0000

    cleaned cart files for the base to be used by everyone

--- e-commerce_project/src/test/java/StepDefinitions/CartSteps.java
+++ e-commerce_project/src/test/java/StepDefinitions/CartSteps.java
@@ -10,22 +10,8 @@ import static StepDefinitions.Hooks.driver;
 
 public class CartSteps {
 
-    @Given("I have an item in the cart")
-    public void iHaveAnItemInTheCart() {
-        //add item to the cart
-        //ex. homePage.clickItem
-        // xxxx.addToCart
-        driver.get("http://3.11.77.136/index.php?controller=cart&action=show");
-    }
-
-    @When("I click bin button next to an item")
-    public void iClickBinButtonNextToAnItem() {
-//        cartPage.clickDeleteBtn;
-        driver.findElement(By.cssSelector("a.remove-from-cart"));
-    }
-
-    @Then("the item disappears and {string} message displays")
-    public void theItemDisappearsAndMessageDisplays(String message) {
-        Assertions.assertEquals(message, driver.findElement(By.cssSelector("span.no-items")).getText());
+    @Given("I navigate to cart with an item in it")
+    public void iNavigateToCartWithAnItemInIt() {
+        //background (waiting for Haider's code)
     }
 }
--- e-commerce_project/src/test/resources/Features/Cart.feature
+++ e-commerce_project/src/test/resources/Features/Cart.feature
@@ -1,11 +1,5 @@
 Feature: Remove products from cart (UI)
 
   Background: add item to cart
-    Given I have an item in the cart
-#    And I'm on cart page
+    Given I navigate to cart with an item in it
 
-
-  Scenario: Remove product from cart
-
-    When I click bin button next to an item
-    Then the item disappears and "There are no more items in your cart" message displays
