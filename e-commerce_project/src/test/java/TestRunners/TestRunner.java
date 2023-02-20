d15602db196d99df2275348e2d1b9f22fb14d241
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-20 12:25:21 +0000

    Added test runner

---
+++ e-commerce_project/src/test/java/TestRunners/TestRunner.java
@@ -0,0 +1,13 @@
+package TestRunners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "src/test/resources/Features", glue = "StepDefinitions")
+public class TestRunner {
+
+
+
+}
