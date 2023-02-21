f443a364d10e8eab79a6546d3d0882e135b205b5
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-21 15:41:59 +0000

    Added a method for choosing color filter

--- e-commerce_project/src/test/java/PageObjects/AllProductsPage.java
+++ e-commerce_project/src/test/java/PageObjects/AllProductsPage.java
@@ -3,10 +3,11 @@ package PageObjects;
 import com.fasterxml.jackson.databind.ser.Serializers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AllProductsPage extends BasePage{
 
-    private static final By ALL_PRODUCTS_BTN = By.xpath("/a[contains(@href,'category=2&controller=category')]");
+    private static final By ALL_PRODUCTS_BTN = By.xpath("//a[contains(@href,'category=2&controller=category')]");
 
     public AllProductsPage(WebDriver driver) {
         super(driver);
@@ -16,4 +17,8 @@ public class AllProductsPage extends BasePage{
         waitUntilVisible(ALL_PRODUCTS_BTN).click();
     }
 
+    public void clickColorFilter(String color) {
+        WebElement selectColor = driver.findElement(By.xpath("//a[contains(text(),'"+color+"')]"));
+        selectColor.click();
+    }
 }
