4366df3186e953752ca5c9a65ad1775099128a4c
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-21 15:04:19 +0000

    Added clickAllProductsBtn method

--- e-commerce_project/src/test/java/PageObjects/AllProductsPage.java
+++ e-commerce_project/src/test/java/PageObjects/AllProductsPage.java
@@ -1,10 +1,19 @@
 package PageObjects;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AllProductsPage extends BasePage{
 
+    private static final By ALL_PRODUCTS_BTN = By.xpath("/a[contains(@href,'category=2&controller=category')]");
+
     public AllProductsPage(WebDriver driver) {
         super(driver);
     }
+
+    public void clickAllProductsBtn(){
+        waitUntilVisible(ALL_PRODUCTS_BTN).click();
+    }
+
 }
