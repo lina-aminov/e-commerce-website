1738c9a31a3ef32adc3864a61eb9f658b020799a
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-21 10:48:29 +0000

    replaced driver.findElement with waitUntilVisible

--- e-commerce_project/src/test/java/PageObjects/CartPage.java
+++ e-commerce_project/src/test/java/PageObjects/CartPage.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class CartPage extends BasePage{
+public class CartPage extends BasePage {
+
 
     private static final By DELETE_ITEM_BTN = By.cssSelector("a.remove-from-cart");
     private static final By NO_ITEM_MSG = By.cssSelector("span.no-items");
@@ -12,11 +13,12 @@ public class CartPage extends BasePage{
     public CartPage(WebDriver driver) {
         super(driver);
     }
-    public void checkEmptyCartMSG(String message){
-        Assertions.assertEquals(message, driver.findElement(NO_ITEM_MSG).getText());
 
+    public void checkEmptyCartMSG(String message) {
+        Assertions.assertEquals(message, waitUntilVisible(NO_ITEM_MSG).getText());
     }
-    public void clickDeleteItemBtn(){
+
+    public void clickDeleteItemBtn() {
         driver.findElement(DELETE_ITEM_BTN).click();
 
     }
