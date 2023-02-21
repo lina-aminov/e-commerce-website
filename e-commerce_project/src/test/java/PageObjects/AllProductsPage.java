57d2c6945cf415921dee34d585a377006bc29c8e
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-21 17:21:25 +0000

    Added methods for selecting filters

--- e-commerce_project/src/test/java/PageObjects/AllProductsPage.java
+++ e-commerce_project/src/test/java/PageObjects/AllProductsPage.java
@@ -1,11 +1,15 @@
 package PageObjects;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
+import io.cucumber.datatable.DataTable;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class AllProductsPage extends BasePage{
+import java.util.Map;
+
+public class AllProductsPage extends BasePage {
 
     private static final By ALL_PRODUCTS_BTN = By.xpath("//a[contains(@href,'category=2&controller=category')]");
 
@@ -13,12 +17,24 @@ public class AllProductsPage extends BasePage{
         super(driver);
     }
 
-    public void clickAllProductsBtn(){
+    public void clickAllProductsBtn() {
         waitUntilVisible(ALL_PRODUCTS_BTN).click();
     }
 
-    public void clickColorFilter(String color) {
-        WebElement selectColor = driver.findElement(By.xpath("//a[contains(text(),'"+color+"')]"));
-        selectColor.click();
+    public void selectFilter(String key, String value) {
+        WebElement selectFilter = driver.findElement(By.xpath("//h1[text()='" + key + "']/parent::section//li//a[contains(text(),'" + value + "')]"));
+        selectFilter.click();
+    }
+
+    public void chooseFilters(DataTable filters) {
+        Map<String, String> filtersMap = filters.asMap();
+        for (Map.Entry<String, String> entry : filtersMap.entrySet()) {
+            selectFilter(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void checkFiltersDisplayed(String filter) {
+        driver.findElement(By.cssSelector(".filter-block")).getText();
+//        Assertions.assertEquals();
     }
 }
