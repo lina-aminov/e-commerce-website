b853ca652c1f13b6bb3dd3616f9d03bbb4b7954f
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-21 17:22:32 +0000

    Added and implemented Step definitions with methods

--- e-commerce_project/src/test/java/StepDefinitions/FiltersAndCategoriesSteps.java
+++ e-commerce_project/src/test/java/StepDefinitions/FiltersAndCategoriesSteps.java
@@ -2,6 +2,8 @@ package StepDefinitions;
 
 import PageObjects.AllProductsPage;
 import PageObjects.CartPage;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static StepDefinitions.Hooks.driver;
@@ -13,9 +15,18 @@ public class FiltersAndCategoriesSteps {
     public FiltersAndCategoriesSteps() {
         allProductsPage = new AllProductsPage(driver);
     }
-    
+
     @When("I go to All Products Page")
     public void iGoToAllProductsPage() {
         allProductsPage.clickAllProductsBtn();
+    }
+
+    @When("I choose filters I want to apply")
+    public void iChooseFiltersIWantToApply(DataTable filters) {
+        allProductsPage.chooseFilters(filters);
+    }
+
+    @Then("the filters are applied")
+    public void theFiltersAreApplied() {
     }
 }
