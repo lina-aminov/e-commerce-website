8f3862fc9c9bc7b0e50c631f09bbb6f31330bd9f
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-21 15:03:50 +0000

    Implemented Background step with clickAllProductsBtn method

--- e-commerce_project/src/test/java/StepDefinitions/FiltersAndCategoriesSteps.java
+++ e-commerce_project/src/test/java/StepDefinitions/FiltersAndCategoriesSteps.java
@@ -2,6 +2,7 @@ package StepDefinitions;
 
 import PageObjects.AllProductsPage;
 import PageObjects.CartPage;
+import io.cucumber.java.en.When;
 
 import static StepDefinitions.Hooks.driver;
 
@@ -12,5 +13,9 @@ public class FiltersAndCategoriesSteps {
     public FiltersAndCategoriesSteps() {
         allProductsPage = new AllProductsPage(driver);
     }
-
+    
+    @When("I go to All Products Page")
+    public void iGoToAllProductsPage() {
+        allProductsPage.clickAllProductsBtn();
+    }
 }
