9e16d8f191c6e3af478e3ccf43d0569e12ff87f4
joel.adjei <jadjei19@gmail.com>
2023-02-22 14:57:06 +0000

    Added steps to checkout

--- e-commerce_project/src/test/java/StepDefinitions/CheckOutStepDefs.java
+++ e-commerce_project/src/test/java/StepDefinitions/CheckOutStepDefs.java
@@ -2,50 +2,80 @@ package StepDefinitions;
 
 import PageObjects.CheckoutPage;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 import static StepDefinitions.Hooks.driver;
 
 public class CheckOutStepDefs {
     CheckoutPage checkoutPage;
-    @Given("I am on the Checkout Page")
+
+
+    public CheckOutStepDefs() {
+        checkoutPage = new CheckoutPage(driver);
+    }
+
+
+    @When("I am on the Checkout Page")
     public void iAmOnTheCheckoutPage() {
-
-    }
-
-
-    @When("I enter my personal details")
-    public void iEnterMyPersonalDetails() {
         checkoutPage = new CheckoutPage(driver);
-        checkoutPage.fillDetails();
-        driver.findElement(By.cssSelector("#customer-form > footer > button")).click();
+
+
     }
 
 
-    @And("I enter my address")
-    public void iEnterMyAddress() {
-        checkoutPage = new CheckoutPage(driver);
-        checkoutPage.fillDetailsAgain();
-        driver.findElement(By.cssSelector("#delivery-address > div > footer > button")).click();
-    }
+
 
     @And("I click on Shipping method")
     public void iClickOnShippingMethod() {
-        driver.findElement(By.cssSelector("#js-delivery > button"));
+        driver.findElement(By.cssSelector("button[name=confirmDeliveryOption]")).click();
     }
 
     @And("I enter payment details by BANK WIRE")
     public void iEnterPaymentDetailsByBANKWIRE() {
-        driver.findElement(By.cssSelector("#payment-option-2"));
-        driver.findElement(By.cssSelector("#conditions_to_approve\\[terms-and-conditions\\]"));
-        driver.findElement(By.cssSelector("#payment-confirmation > div.ps-shown-by-js > button"));
+        driver.findElement(By.cssSelector("#payment-option-2")).click();
+        driver.findElement(By.cssSelector("input[name=\"conditions_to_approve[terms-and-conditions]\"]")).click();
+
     }
 
     @Then("I click on Order to Pay")
     public void iClickOnOrderToPay() {
-        driver.findElement(By.cssSelector("#payment-confirmation > div.ps-shown-by-js > button"));
+        driver.findElement(By.cssSelector("button.btn.btn-primary.center-block[type=submit]")).click();
+    }
+
+    @And("I click proceed to checkout again")
+    public void iClickProceedToCheckoutAgain() {
+        driver.findElement(By.cssSelector("#main > div > div.cart-grid-right.col-xs-12.col-lg-4 > div.card.cart-summary > div.checkout.cart-detailed-actions.card-block > div > a")).click();
+    }
+
+    @And("I enter my personal details like firstname {string} lastname {string} email {string}")
+    public void iEnterMyPersonalDetailsLikeFirstnameLastnameEmail(String firstname, String lastname, String email) {
+        checkoutPage.enterfirstname(firstname);
+        checkoutPage.enterlastname(lastname);
+        checkoutPage.enteremail(email);
+
+        driver.findElement(By.cssSelector("button[name=continue]")).click();
+    }
+
+    @And("I enter my address {string} city {string} zipcode {string}")
+    public void iEnterMyAddressCityZipcode(String address, String city, String zipcode) {
+            checkoutPage.enteraddress(address);
+            checkoutPage.entercity(city);
+        Select drpState = new Select(driver.findElement(By.name("id_state")));
+        drpState.selectByVisibleText("Delaware");
+            checkoutPage.enterzipcode(zipcode);
+
+            driver.findElement(By.cssSelector("button[name=confirm-addresses]")).click();
+        }
+
+    @Then("Website says incorrect zipcode format")
+    public void websiteSaysIncorrectZipcodeFormat()  {
+
+
     }
 }
+
+
+
