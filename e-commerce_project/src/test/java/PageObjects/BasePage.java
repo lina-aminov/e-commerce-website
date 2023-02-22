498a639c5e371dbad1decd538436463e97f8521b
lina.aminov <lina.aminov@ten10.com>
2023-02-22 12:44:17 +0000

    Added cart quantity tests and logging file

--- e-commerce_project/src/test/java/PageObjects/BasePage.java
+++ e-commerce_project/src/test/java/PageObjects/BasePage.java
@@ -13,7 +13,7 @@ abstract class BasePage {
     WebDriver driver;
     WebDriverWait wait;
 
-    BasePage(WebDriver driver){
+    BasePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
@@ -21,8 +21,17 @@ abstract class BasePage {
     public void waitAndClick(By selector) {
         wait.until(ExpectedConditions.elementToBeClickable(selector)).click();
     }
+
     public WebElement waitUntilVisible(By selector) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(selector));
+    }
+
+    public void waitAndClickForStale(By selector) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(selector));
+        element.click();
+        wait.until(ExpectedConditions.stalenessOf(element));
+
+
     }
 
 
