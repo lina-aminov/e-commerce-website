6eb944a667994ce59bd2cee529fab1a24ea29754
patrycja.ciosek <patrycja.ciosek@ten10.com>
2023-02-22 12:19:35 +0000

    Added waitUntilStale method

--- e-commerce_project/src/test/java/PageObjects/BasePage.java
+++ e-commerce_project/src/test/java/PageObjects/BasePage.java
@@ -43,5 +43,8 @@ abstract class BasePage {
         int range = max - min + 1;
         return (int) (Math.random() * range) + min;
     }
+    public void waitUntilStale(WebElement element){
+        wait.until(ExpectedConditions.stalenessOf(element));
+    }
 
 }
