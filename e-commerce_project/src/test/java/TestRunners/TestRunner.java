032ca3ba67caee4bd77f5fc387203d9586c61af5
lina.aminov <lina.aminov@ten10.com>
2023-02-23 08:37:17 +0000

    Clucumber reporting for the tests with plugins and dependencies

--- e-commerce_project/src/test/java/TestRunners/TestRunner.java
+++ e-commerce_project/src/test/java/TestRunners/TestRunner.java
@@ -5,9 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/resources/Features", glue = "StepDefinitions")
+@CucumberOptions(features = "src/test/resources/features", glue = {"StepDefinitions"}, plugin  = {"json:target/cucumber-report/cucumber.json"})
 public class TestRunner {
-
 
 
 }
